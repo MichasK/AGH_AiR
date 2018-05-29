8009c5bb8e1b9544c17b91ebf3a89ea5d20dcd79
Michał Krzyszczuk <krzyszczuk.michal@gmail.com>
2018-05-29 21:04:40 +0200

    SW lab 12 set up directory and files

---
+++ [Sem4]/Systemy Wizyjne/LAB12.Przekształcenia morfologiczne/podstawowe_operacje_morfologiczne.m
@@ -0,0 +1,28 @@
+close all;
+clear all;
+
+ertka = imread('pliki/ertka.bmp');
+
+SE_square = strel('square',3);
+
+SE_diamond = strel('diamond',3);
+
+SE_disk = strel('disk',4,6);
+
+ertka_erozja_square = imerode(ertka,SE_square);
+ertka_erozja_diamond = imerode(ertka,SE_diamond);
+ertka_erozja_disk = imerode(ertka,SE_disk);
+
+figure('name','ERTKA erozja kwadrat 3x3')
+subplot(2,2,1)
+imshow(ertka);
+title('Input image')
+subplot(2,2,2)
+imshow(ertka_erozja_square);
+title('Erozja 3x3 kwadrat')
+subplot(2,2,3)
+imshow(ertka_erozja_diamond);
+title('Erozja diamond')
+subplot(2,2,4)
+imshow(ertka_erozja_disk);
+title('Erozja disk')
