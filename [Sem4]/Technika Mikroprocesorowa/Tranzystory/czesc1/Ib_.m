a718c307ba2a9a77e1ce9d2260f4ca303dbdb19f
Michał Krzyszczuk <krzyszczuk.michal@gmail.com>
2018-05-30 13:31:27 +0200

    Poprawa charakterystyki Id(Ude)

--- [Sem4]/Technika Mikroprocesorowa/Tranzystory/czesc1/Ib_.m
+++ [Sem4]/Technika Mikroprocesorowa/Tranzystory/czesc1/Ib_.m
@@ -5,12 +5,12 @@ Ib = xlsread('Dane/IB_UBE.xls','E2:E22');
 Ube = xlsread('Dane/IB_UBE.xls','F2:F22');
 
 Ib_model = xlsread('Dane/uklad1.xlsx','B2:B22');
-Ube_model = xlsread('Dane/uklad1.xlsx','D2:C22');
+Ube_model = xlsread('Dane/uklad1.xlsx','D2:D22');
 
 figure(1)
 plot(Ube,Ib,'DisplayName','I_b');
 hold on;
-plot(Ube_model,Ib_model,'DisplayName','Ib_model');
+plot(Ube_model,-Ib_model,'DisplayName','Ib_model');
 legend show;
 grid on
 legend('Location','best')
